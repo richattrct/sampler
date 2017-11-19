0946169d6aec0108d518f135964cbd953177662b
Richard Ankers <rankers@deloitte.co.uk>
2017-11-19 13:23:54 +0000

    Initial Commit

---
+++ sampler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampler
+//
+//  Created by Ankers, Richard (UK - London) on 11/19/17.
+//  Copyright © 2017 Ankers, Richard (UK - London). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
